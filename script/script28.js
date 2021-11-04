9bc68e545629d0317afd0850f98c84410b4a5837
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2021-11-04 10:15:02 +0300

    Add files via upload
    Второе изменение

--- script/script28.js
+++ script/script28.js
@@ -1,2 +1,12 @@
-let title = document.getElementsByClassName('main-card-title');
-console.log(title[1].innerHTML)
+// let title = document.getElementsByClassName('main-card-title');
+// console.log(title[1].innerHTML)
+
+let addBtn = document.getElementsByClassName('add-to-card');
+let myFunction = function () {
+    let title = this.parentNode.parentNode.getElementsByClassName('main-card-title');
+    console.log(title[0].innerText);
+}
+
+for (let i = 0; i < addBtn.length; i++) {
+    addBtn[i].addEventListener('click', myFunction, false)
+}
